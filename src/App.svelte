0db505656d8b238df0ba715bacb3c9364b0ac7a4
bagstel <stelios@baglaridis.com>
2022-10-25 18:36:20 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+  const [id] = window.location.pathname.split("/").filter(a => !!a)
+  console.log(id)
+</script>
+
+<div class="text-2xl">{id}</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,svelte}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
